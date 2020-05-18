70e0401744e175593a7745286f8b91667fb1f9fd
Aimeos GmbH <dev@aimeos.com>
2020-05-18 15:14:43 +0200

    Implements service provider decorators for delivery/payment options

---
+++ lib/custom/src/MShop/Service/Provider/Decorator/Carrier.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @copyright Aimeos GmbH, 2020
+ * @package MShop
+ * @subpackage Service
+ */
+
+
+namespace Aimeos\MShop\Service\Provider\Decorator;
+
+
+/**
+ * Decorator to check if carrier transport is necessary.
+ *
+ * @package MShop
+ * @subpackage Service
+ */
+class Carrier
+	extends \Aimeos\MShop\Service\Provider\Decorator\Base
+	implements \Aimeos\MShop\Service\Provider\Decorator\Iface
+{
+	public function isAvailable( \Aimeos\MShop\Order\Item\Base\Iface $basket )
+	{
+		$prodIds = [];
+		foreach( $basket->getProducts() as $orderProduct ) {
+			$prodIds[] = $orderProduct->getProductId();
+		}
+
+		$manager = \Aimeos\MShop::create( $this->getContext(), 'product' );
+		$search = $manager->createSearch()->setSlice( 0 , 1 );
+		$search->setConditions( $search->combine( '&&', [
+			$search->compare( '==', 'product.id', $prodIds ),
+			$search->compare( '!=', $search->createFunction( 'product:prop', ['shipping', 'carrier'], null ) )
+		] ) );
+
+		return count( $manager->searchItems( $search ) ) ? $this->getProvider()->isAvailable( $basket ) : false;
+	}
+}
