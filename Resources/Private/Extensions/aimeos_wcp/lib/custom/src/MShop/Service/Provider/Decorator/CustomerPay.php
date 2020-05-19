c5de27b8c593bd938a29b8e968a33f1f46ebecf2
Ulrich Diehl <ulrichdiehl@gmail.com>
2020-05-19 11:34:43 +0200

    Move files

---
+++ Resources/Private/Extensions/aimeos_wcp/lib/custom/src/MShop/Service/Provider/Decorator/CustomerPay.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @copyright Aimeos GmbH, 2020
+ * @package MShop
+ * @subpackage Service
+ */
+
+
+namespace Aimeos\MShop\Service\Provider\Decorator;
+
+
+/**
+ * Decorator to check if payment is available for customer.
+ *
+ * @package MShop
+ * @subpackage Service
+ */
+class CustomerPay
+	extends \Aimeos\MShop\Service\Provider\Decorator\Base
+	implements \Aimeos\MShop\Service\Provider\Decorator\Iface
+{
+	private $beConfig = array(
+		'customerpay.payment' => array(
+			'code' => 'customerpay.payment',
+			'internalcode' => 'customerpay.payment',
+			'label' => 'Required payment',
+			'type' => 'string',
+			'internaltype' => 'string',
+			'default' => '',
+			'required' => true,
+		),
+	);
+
+
+	public function checkConfigBE( array $attributes )
+	{
+		$error = $this->getProvider()->checkConfigBE( $attributes );
+		$error += $this->checkConfig( $this->beConfig, $attributes );
+
+		return $error;
+	}
+
+
+	public function getConfigBE()
+	{
+		return array_merge( $this->getProvider()->getConfigBE(), $this->getConfigItems( $this->beConfig ) );
+	}
+
+
+	public function isAvailable( \Aimeos\MShop\Order\Item\Base\Iface $basket )
+	{
+		$context = $this->getContext();
+		$item = \Aimeos\MShop::create( $context, 'customer' )->findItem( $context->getUserId(), ['customer/property'] );
+		$option = current( $item->getProperties( 'payment' ) );
+
+		if( $option && $option !== $this->getConfigValue( 'customerpay.payment' ) ) {
+			return false;
+		}
+
+		return $this->getProvider()->isAvailable( $basket );
+	}
+}
