742e6adf1cdc548c889e37c98ab8bf91e49d2091
Aimeos GmbH <dev@aimeos.com>
2020-08-04 17:19:15 +0200

    Changes Solr core config for live

--- Resources/Private/Config/resource.php
+++ Resources/Private/Config/resource.php
@@ -6,6 +6,6 @@ return [
 		'host' => '5146153:lxsFGBJeaW9vIJdTXnMvxJJeiOpQCDx5@5146153.solr-02.jweiland.net',
 		'port' => '443',
 		'path' => 'solr',
-		'index' => 'core_de_aimeos',
+		'index' => 'core_de_aimeos_2',
 	]
 ];
