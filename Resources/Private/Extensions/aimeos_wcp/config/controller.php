879b4f768dd026290b9410647fc28591002f9b11
Aimeos GmbH <dev@aimeos.com>
2020-08-04 17:08:14 +0200

    Merges config files

--- Resources/Private/Extensions/aimeos_wcp/config/controller.php
+++ Resources/Private/Extensions/aimeos_wcp/config/controller.php
@@ -1,10 +1,23 @@
 <?php
 
 return [
-	'common' => [
-	],
-	'frontend' => [
-	],
-	'jobs' => [
-	],
+    'common' => [
+        'media' => [
+            'standard' => [
+                'previews' => [[
+                    'maxwidth' => 240,
+                    'maxheight' => 180,
+                    'force-size' => false,
+                ], [
+                    'maxwidth' => 650,
+                    'maxheight' => 488,
+                    'force-size' => false,
+                ], [
+                    'maxwidth' => 1300,
+                    'maxheight' => 976,
+                    'force-size' => false,
+                ]],
+            ],
+        ],
+    ],
 ];
--- Resources/Private/Extensions/aimeos_wcp/config/mshop.php
+++ Resources/Private/Extensions/aimeos_wcp/config/mshop.php
@@ -9,5 +9,41 @@ return [
 				]
 			]
 		]
-	]
+	],
+	'customer' => [
+		'manager' => [
+			'typo3' => [
+				'pid-default' => 87
+			],
+		],
+	],
+	'index' => [
+		'manager' => [
+			'name' => 'Solr',
+			'standard' => [
+				'domains' => [
+					'catalog' => 'catalog',
+					'attribute' => 'attribute',
+					'product' => ['default'],
+					'price' => ['default'],
+					'text' => 'text',
+				],
+			],
+			'attribute' => [
+				'name' => 'Solr'
+			],
+			'catalog' => [
+				'name' => 'Solr'
+			],
+			'price' => [
+				'name' => 'Solr'
+			],
+			'text' => [
+				'name' => 'Weber'
+			]
+		]
+	],
+	'price' => [
+		'taxflag' => 0,
+	],
 ];
