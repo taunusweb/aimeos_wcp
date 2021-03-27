e30ee0dbb89efed2826453fef8dd7c3cd5a9b621
Aimeos GmbH <dev@aimeos.com>
2021-03-27 17:32:56 +0100

    Fix for retrieving customer payment property

--- Resources/Private/Extensions/aimeos_wcp/lib/custom/src/MShop/Service/Provider/Decorator/CustomerPay.php
+++ Resources/Private/Extensions/aimeos_wcp/lib/custom/src/MShop/Service/Provider/Decorator/CustomerPay.php
@@ -55,7 +55,7 @@ class CustomerPay
 		if( $context->getUserId() )
 		{
 			$item = \Aimeos\MShop::create( $context, 'customer' )->getItem( $context->getUserId(), ['customer/property'] );
-			$option = $item->getProperties( 'payment' )->first();
+			$option = current($item->getProperties( 'payment' ));
 
 			if( $option && $option === $this->getConfigValue( 'customerpay.payment' ) ) {
 				return false;
