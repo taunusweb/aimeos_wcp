94eac731d1a8e6f0008079ba7f102a25be3352f8
Aimeos GmbH <dev@aimeos.com>
2022-03-29 14:15:36 +0200

    Fixes namespace

--- Resources/Private/Extensions/aimeos_wcp/lib/custom/src/MShop/Service/Provider/Delivery/XmlWeber.php
+++ Resources/Private/Extensions/aimeos_wcp/lib/custom/src/MShop/Service/Provider/Delivery/XmlWeber.php
@@ -203,7 +203,7 @@ class XmlWeber
 	protected function createFile( string $content )
 	{
 		$filepath = $this->getConfigValue( 'xml.exportpath', './order_%Y-%m-%d_%H:%i:%s_%v.xml' );
-		$filepath = sprintf( \Aimeos\Base\Str::strtime( $filepath ), $this->num++ );
+		$filepath = sprintf( \Aimeos\MW\Str::strtime( $filepath ), $this->num++ );
 
 		if( file_put_contents( $filepath, $content ) === false )
 		{
