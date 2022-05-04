31e943712d1c120b4f802acf86a4dcf3f12080ac
Aimeos GmbH <dev@aimeos.com>
2022-05-04 14:30:25 +0200

    Uses case-insentitive search again

--- Resources/Private/Extensions/aimeos_wcp/lib/custom/src/MShop/Index/Manager/Text/Weber.php
+++ Resources/Private/Extensions/aimeos_wcp/lib/custom/src/MShop/Index/Manager/Text/Weber.php
@@ -76,17 +76,17 @@ class Weber extends \Aimeos\MShop\Index\Manager\Text\Solr
 
 			$text = addcslashes( $params[1], '+-&|!(){}[]^"~*?:/' );
 
-			$source['bool']['should'][] = 'product.code:"' . $text . '"^1000';
-			$source['bool']['should'][] = 'product.code:"' . $text . '*"^750';
+			$source['bool']['should'][] = 'product.code:(' . $text . ')^1000';
+			$source['bool']['should'][] = 'product.code:(' . $text . '*)^750';
 
 			$text = str_replace( ['.', '-', ',', '"', '\\'], '', $text );
 
-			$source['bool']['should'][] = 'index.text.name_' . $params[0] . ':"' . $text . '"^450';
-			$source['bool']['should'][] = 'index.text.name_' . $params[0] . ':"' . $text . '*"^300';
-			$source['bool']['should'][] = 'index.text.content_' . $params[0] . ':"' . $text . '"^150';
-			$source['bool']['should'][] = 'index.text.content_' . $params[0] . ':"' . $text . '*"^50';
-			$source['bool']['should'][] = 'index.text.content_:"' . $text . '"^800';
-			$source['bool']['should'][] = 'index.text.content_:"' . $text . '*"^600';
+			$source['bool']['should'][] = 'index.text.name_' . $params[0] . ':(' . $text . ')^450';
+			$source['bool']['should'][] = 'index.text.name_' . $params[0] . ':(' . $text . '*)^300';
+			$source['bool']['should'][] = 'index.text.content_' . $params[0] . ':(' . $text . ')^150';
+			$source['bool']['should'][] = 'index.text.content_' . $params[0] . ':(' . $text . '*)^50';
+			$source['bool']['should'][] = 'index.text.content_:(' . $text . ')^800';
+			$source['bool']['should'][] = 'index.text.content_:(' . $text . '*)^600';
 
 			return $params;
 		};
