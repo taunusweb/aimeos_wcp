464aadb1933802019299b6911aa245836f75dd86
Aimeos GmbH <dev@aimeos.com>
2022-11-30 17:57:10 +0100

    Filters categories in list view for short terms

--- Resources/Private/Extensions/aimeos_wcp/client/html/src/Client/Html/Common/Decorator/ListCategories.php
+++ Resources/Private/Extensions/aimeos_wcp/client/html/src/Client/Html/Common/Decorator/ListCategories.php
@@ -12,7 +12,19 @@ class ListCategories
 
 		if( $text = $view->param( 'f_search' ) )
 		{
-			$view->listNodes = \Aimeos\Controller\Frontend::create( $this->getContext(), 'catalog' )->uses( ['media'] )
+			$cntl = \Aimeos\Controller\Frontend::create( $context, 'catalog' );
+
+			foreach( explode( ' ', $text ) as $str )
+			{
+				$len = strlen( $str );
+				$str = preg_filter( '/[A-Za-z0-9]/', '$0', $str );
+
+				if( strlen( $str ) > 0 && $len < 4 ) {
+					$cntl->compare( '~=', 'catalog.label', $str );
+				}
+			}
+
+			$view->listNodes = $cntl->uses( ['media'] )
 				->compare( '>', 'catalog:relevance("' . str_replace( ['"', ','], ' ', $text ) . '")', 0 )
 				->sort( '-sort:catalog:relevance("' . str_replace( ['"', ','], ' ', $text ) . '")' )->sort( 'catalog.label' )
 				->slice( 0, 8 )->search();
