0af935cbf8ebaf76b4612edd95cabbed0a45f5d1
Aimeos GmbH <dev@aimeos.com>
2023-07-26 19:11:34 +0200

    Uses variables from LocalConfiguration for Solr config

--- Resources/Private/Config/resource.php
+++ Resources/Private/Config/resource.php
@@ -2,10 +2,10 @@
 
 return [
 	'solr' => [
-		'scheme' => 'https',
-		'host' => 'solr6269:yJcs67JK@solr6269.solr-hosting.info',
-		'port' => '443',
-		'path' => 'solr',
-		'index' => 'aimeos_2022',
+		'scheme' => $GLOBALS['TYPO3_CONF_VARS']['SOLR']['scheme'] ?? 'https',
+		'host' => $GLOBALS['TYPO3_CONF_VARS']['SOLR']['host'] ?? 'solr6269:yJcs67JK@solr6269.solr-hosting.info',
+		'port' => $GLOBALS['TYPO3_CONF_VARS']['SOLR']['port'] ?? '443',
+		'path' => $GLOBALS['TYPO3_CONF_VARS']['SOLR']['path'] ?? 'solr',
+		'index' => $GLOBALS['TYPO3_CONF_VARS']['SOLR']['index'] ?? 'aimeos_2022',
 	]
 ];
